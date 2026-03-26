e7da5f4bfffbac0703a0de334eac0a10b903a5fd
gripexdev <cj.othman@gmail.com>
2026-03-26 21:15:28 +0100

    update discord invite link

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -2,4 +2,4 @@ export const SERVER_NAME = 'The Village S3'
 export const SERVER_IP = 'thevillages3.ultraga.me'
 export const VOTE_URL =
   'https://linkvertise.com/4451625/nK8gHjt99uFI?o=sharing'
-export const DISCORD_URL = 'https://discord.com/invite/bX9DY5Bu'
+export const DISCORD_URL = 'https://discord.gg/eKzbBurQSm'
